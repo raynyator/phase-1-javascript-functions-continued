0a6bd3d02f5ecd16bbe85f7396338471ddd3cf67
raynyator <rayn.yator@student.moringaschool.com>
2023-09-23 14:40:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+
+    return"This Saturday, I want to roller-skate!" 
+}
+function mondayWork(){
+
+    return"This Monday, I will go to the office."
+   
+}
+function wrapAdjective(style="*"){
+    return function(adj="a hard worker"){
+        return "You are *a hard worker*!"
+    
+    }
+
+}
+
+
+
+
+
+
+
+     
